904057053607dc1f16634d694c8f8ec65aa55dcb
ysrotciv <ysrotciv@gmail.com>
2016-08-24 20:40:44 +0800

    add type enum

---
+++ src/main/scala/com/kunyandata/backtesting/config/FilterType.scala
@@ -0,0 +1,11 @@
+package com.kunyandata.backtesting.config
+
+/**
+  * Created by YangShuai
+  * Created on 2016/8/24.
+  */
+object FilterType extends Enumeration {
+
+  val Example = Value(Int.MaxValue, "EXAMPLE")
+
+}

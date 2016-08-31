793cdbdb3363d328d32053eca53d0eb0dab450bd
ysrotciv <ysrotciv@gmail.com>
2016-08-31 11:45:53 +0800

    add str to offset method in DateUtil

--- src/main/scala/com/kunyandata/backtesting/util/DateUtil.scala
+++ src/main/scala/com/kunyandata/backtesting/util/DateUtil.scala
@@ -1,7 +1,6 @@
 package com.kunyandata.backtesting.util
 
 import java.text.SimpleDateFormat
-import java.util.Calendar
 
 /**
   * Created by YangShuai
@@ -9,14 +8,27 @@ import java.util.Calendar
   */
 object DateUtil {
 
+  val DATE_FORMAT = "yyyy-MM-dd"
+
   /**
     * 获得yyyy-MM-dd格式的代表日期的字符串
-    * @param offset 偏差值，如果获得昨天的日期字符串写-1
+    * @param offset 偏差值，如果需获得昨天的日期字符串则传-1
     * @return
     */
   def getDateStr(offset: Int): String = {
     val timeStamp = System.currentTimeMillis() + offset * 24 * 60 * 60 * 1000
-    new SimpleDateFormat("yyyy-MM-dd").format(timeStamp)
+    new SimpleDateFormat(DATE_FORMAT).format(timeStamp)
+  }
+
+  /**
+    * 获得给定字符串所代表的日期与当日的偏差值
+    * 例如：若给的是昨天则返回-1
+    * @param dateString 代表日期的字符串（"yyyy-MM-dd"）
+    * @return 给定字符串所代表的日期与当日的偏差值
+    */
+  def getOffset(dateString: String): Int = {
+    val timeStamp = new SimpleDateFormat(DATE_FORMAT).parse(dateString).getTime
+    ((timeStamp - System.currentTimeMillis()) / (24 * 60 * 60 * 1000)).toInt
   }
 
 }

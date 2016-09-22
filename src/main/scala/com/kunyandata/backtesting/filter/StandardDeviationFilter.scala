47ea7d8bfa254bf0db765f36a2453509e6a321b0
tianxingjian <sijiansheng@kunyan-inc.com>
2016-09-22 16:11:21 +0800

    修改参数

--- src/main/scala/com/kunyandata/backtesting/filter/StandardDeviationFilter.scala
+++ src/main/scala/com/kunyandata/backtesting/filter/StandardDeviationFilter.scala
@@ -11,42 +11,39 @@ import scala.collection.mutable
 /**
   * Created by sijiansheng on 2016/9/21.
   */
-class StandardDeviationFilter private(prefix: String, multiple: Double, meanCriterion: Int, stdCriterion: Int, start: Int, end: Int) extends Filter {
+class StandardDeviationFilter private(prefix: String, multiple: Double, meanCriterion: Int, stdCriterion: Int, day: Int) extends Filter {
 
   override def filter(): List[String] = {
 
     var resultSet = mutable.Set[String]()
     val jedis = RedisHandler.getInstance().getJedis
 
-    for (i <- start to end) {
+    val date = CommonUtil.getDateStr(day)
+    val key = prefix + date
+    val valuesAndScores = jedis.zrangeByScoreWithScores(prefix + date, Double.MinValue, Double.MaxValue)
+    val meanValuesAndScoresMap = valueAndScoreToMap(jedis.zrangeByScoreWithScores("heat_mean_" + meanCriterion + "_" + date, Double.MinValue, Double.MaxValue))
+    val standardDeviationValuesAndScoresMap = valueAndScoreToMap(jedis.zrangeByScoreWithScores("heat_std_" + stdCriterion + "_" + date, Double.MinValue, Double.MaxValue))
 
-      val date = CommonUtil.getDateStr(i)
-      val key = prefix + date
-      val valuesAndScores = jedis.zrangeByScoreWithScores(prefix + date, Double.MinValue, Double.MaxValue)
-      val meanValuesAndScoresMap = valueAndScoreToMap(jedis.zrangeByScoreWithScores("heat_mean_" + meanCriterion + "_" + date, Double.MinValue, Double.MaxValue))
-      val standardDeviationValuesAndScoresMap = valueAndScoreToMap(jedis.zrangeByScoreWithScores("heat_std_" + stdCriterion + "_" + date, Double.MinValue, Double.MaxValue))
+    val iterator = valuesAndScores.iterator()
 
-      val iterator = valuesAndScores.iterator()
+    while (iterator.hasNext) {
 
-      while (iterator.hasNext) {
+      val valueAndScore = iterator.next()
+      val value = valueAndScore.getElement
+      val score = valueAndScore.getScore
+      val meanScore = meanValuesAndScoresMap.getOrElse(value, Double.MaxValue)
+      val standardDeviationScore = standardDeviationValuesAndScoresMap.getOrElse(value, Double.MaxValue)
 
-        val valueAndScore = iterator.next()
-        val value = valueAndScore.getElement
-        val score = valueAndScore.getScore
-        val meanScore = meanValuesAndScoresMap.getOrElse(value, Double.MaxValue)
-        val standardDeviationScore = standardDeviationValuesAndScoresMap.getOrElse(value, Double.MaxValue)
+      if (meanScore != Double.MaxValue && standardDeviationScore != Double.MaxValue) {
 
-        if (meanScore != Double.MaxValue && standardDeviationScore != Double.MaxValue) {
-
-          if (score.toDouble > (multiple * standardDeviationScore + meanScore)) {
-            resultSet += value
-          }
-
+        if (score.toDouble > (multiple * standardDeviationScore + meanScore)) {
+          resultSet += value
         }
 
       }
 
     }
+
 
     jedis.close()
     resultSet.toList
@@ -70,9 +67,9 @@ class StandardDeviationFilter private(prefix: String, multiple: Double, meanCrit
 
 object StandardDeviationFilter {
 
-  def apply(prefix: String, multiple: Double, meanCriterion: Int, stdCriterion: Int, start: Int, end: Int): StandardDeviationFilter = {
+  def apply(prefix: String, multiple: Double, meanCriterion: Int, stdCriterion: Int, day: Int): StandardDeviationFilter = {
 
-    val filter = new StandardDeviationFilter(prefix, multiple, meanCriterion, stdCriterion, start: Int, end: Int)
+    val filter = new StandardDeviationFilter(prefix, multiple, meanCriterion, stdCriterion, day: Int)
 
     filter.futureTask = new FutureTask[List[String]](new Callable[List[String]] {
       override def call(): List[String] = filter.filter()
